17f04d9833f8ac1a6fe4065481ea2cf35fa132f2
Ugwoke levi <113690452+levoski1@users.noreply.github.com>
2024-06-23 16:37:57 +0100

    feat{urls.py}: Added path for authentification

--- App/urls.py
+++ App/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from App import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),    
 ]
