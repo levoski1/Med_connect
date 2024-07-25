8e7a12436cd8cd90b75c3ae9bfb7637960fe7895
Ugwoke levi <soromtolevi1@gmail.com>
2024-07-25 08:46:46 +0100

    ok

--- django_project/urls.py
+++ django_project/urls.py
@@ -32,6 +32,6 @@ urlpatterns = [
     path('password_reset_complete/', views.PasswordResetComplete.as_view(), name='password_reset_complete'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
