57b29b9d70c7429fc2aef1bd8cc210e47f8df8bf
Ugwoke levi <soromtolevi1@gmail.com>
2024-07-25 18:42:55 +0100

    okay

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,7 @@
 # Install dependencies
 python3.9 -m ensurepip --upgrade 
 
-pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 # Run any build commands
 python3.9 manage.py collectstatic --noinput
